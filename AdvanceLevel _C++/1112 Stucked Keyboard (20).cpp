93223a533834a38de1efabe934e0f0f8636852bb
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2019-08-15 15:19:18 +0800

    Update 1112 Stucked Keyboard (20).cpp

--- AdvanceLevel _C++/1112 Stucked Keyboard (20).cpp
+++ AdvanceLevel _C++/1112 Stucked Keyboard (20).cpp
@@ -1,73 +1,33 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
 #include <vector>
 #include <map>
-#include <math.h>
-#include <string>
-#include <string.h>
-#include <algorithm>
-
 using namespace std;
-bool find(vector<char> res,char c){
-	for(int i=0;i<res.size();i++){
-		if(res[i]==c) return false;
-	} 
-	return true;
-}
 
 int main(){
-//	freopen("1.txt","r",stdin);
-	string str;
-	int n,len;
-	map<char,int> mp;
-	map<char,int> res;
-	vector<char> re; 
-	cin>>n>>str;
-	len=str.length();
-	
-	for(int i=0;i<len-1;i++){
-		if(str[i]!=str[i+1]){
-			mp[str[i]]=1;
-		}
-		else if(mp[str[i]]==1){
-			continue;
-		}else{
-			int j;
-			for(j=0;j<n;j++){
-				if(i+j>=len) break;
-				if(str[i+j]!=str[i]) break;
-			}
-			if(j==n){
-				 mp[str[i]]=-1;
-				 i=i+n-1;
-			}
-		}
-	}
-	
-	for(int i=0;i<len;i++){
-		if(mp[str[i]]==-1){
-			if(res[str[i]]!=1){
-				res[str[i]]=1;
-			}
-			if(find(re,str[i])){
-				re.push_back(str[i]);
-			}
-			i=i+n-1;
-		}
-	}
-//	for(map<char,int>::iterator it=res.begin();it!=res.end();it++){
-//		printf("%c",it->first);
-//	}
-	for(int i=0;i<re.size();i++){
-		printf("%c",re[i]);
-	}
-	cout<<endl;
-	for(int i=0;i<len;i++){
-		printf("%c",str[i]);
-		if(res[str[i]]==1){
-			i=i+n-1;
-		}
-		else{
-		}
-	}
+//    freopen("1.txt","r",stdin);
+    string str;
+    int n,i,j,len;
+    map<char,int> tag,in;
+    cin>>n>>str;
+    len=str.length();
+    vector<char> res;
+    for(i=0;i<len;i=j){
+        j=i;
+        while(j<len&&str[j]==str[i]) j++;
+        if((j-i)%n!=0) tag[str[i]]=1;
+    }
+    for(i=0;i<len;i++){
+        if(tag[str[i]]==0&&in[str[i]]==0){
+           res.push_back(str[i]);
+           in[str[i]]=1;
+        }
+    }
+    for(i=0;i<res.size();i++) printf("%c",res[i]);
+    printf("\n");
+    for(i=0;i<len;i++){
+        if(tag[str[i]]==0) i=i+n-1;
+        printf("%c",str[i]);
+    }
 }
