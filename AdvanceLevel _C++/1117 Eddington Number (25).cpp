e83d78772d59ed6760b15d47acab4cc48a155e2a
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2019-08-20 19:40:08 +0800

    Update 1117 Eddington Number (25).cpp

--- AdvanceLevel _C++/1117 Eddington Number (25).cpp
+++ AdvanceLevel _C++/1117 Eddington Number (25).cpp
@@ -1,36 +1,19 @@
-#include <stdio.h>
 #include <iostream>
 #include <vector>
-#include <math.h>
-#include <string>
-#include <string.h>
 #include <algorithm>
-#include <map>
-
 using namespace std;
-
-bool cmp(int a,int b){
-	return a>b;
-}
-
 int main(){
-	
-	int n,temp;
-	vector<int> a;
-	cin>>n;
-	for(int i=0;i<n;i++){
-		scanf("%d",&temp);
-		a.push_back(temp);
-	}
-	sort(a.begin(),a.end(),cmp);
-	for(int i=0;i<a.size();i++){
-//		printf("%d ",a[i]);
-	}
-	for(int i=0;i<=n;i++){
-		if(a[i]<=i+1){
-			printf("%d",i);
-			return 0;
-		}
-	}
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a.rbegin(),a.rend());
+    for(int i=0;i<n;i++){
+        if(a[i]<=i+1){
+            cout<<i;
+            return 0;
+        }
+    }
+    cout<<n;
+    return 0;
 }
-
