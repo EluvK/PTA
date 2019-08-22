eb93f9fd84909068891a408af14b1e6dd2779cd6
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2019-08-22 22:05:21 +0800

    Update 1002 A+B for Polynomials (25).cpp

--- AdvanceLevel _C++/1002 A+B for Polynomials (25).cpp
+++ AdvanceLevel _C++/1002 A+B for Polynomials (25).cpp
@@ -1,36 +1,26 @@
 #include <stdio.h>
-#include <math.h>
-int main(){
-	double coeff[1001]={0};
-	int num;
-	int exp;
-	double coe;
-	scanf("%d",&num);
-	for(int i=0;i<num;i++){
-		scanf("%d %lf",&exp,&coe);
-		coeff[exp]+=coe;
-	}
-	scanf("%d",&num);
-	for(int i=0;i<num;i++){
-		scanf("%d %lf",&exp,&coe);
-		coeff[exp]+=coe;
-	}
-	int shownum=0;
-	for(int i=0;i<1001;i++){
-		if(fabs(coeff[i]-0.0)>10e-5){
-			shownum++;
-		}
-	}
-	printf("%d",shownum);
-	if(shownum){
-		for(int i=1000;i>=0;i--){
-			if(fabs(coeff[i]-0.0)>10e-5){
-				printf(" %d %.1f",i,coeff[i]);
-			}
-		}
-	}
-	
-	return 0;
-	
-} 
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main(){
+//    freopen("1.txt","r",stdin);
+    int n,temp,cnt=0,mx=0,tt=2;
+    double e[1010]={0.0},ipt;
+    while(tt--){
+        scanf("%d",&n);
+        for(int i=0;i<n;i++){
+            scanf("%d %lf",&temp,&ipt);
+            e[temp]+=ipt;
+            mx=max(temp,mx);
+        }
+    }
+    for(int i=0;i<=mx;i++) if(e[i]!=0) cnt++;
+    printf("%d",cnt);
+    for(int i=mx;i>=0;i--){
+        if(e[i]!=0){
+            printf(" %d %.1lf",i,e[i]);
+        }
+    }
+    return 0;
+}
