e87e20b737bb57f6c3701b0b54990e2fce24ca86
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2019-08-22 15:13:01 +0800

    Update 1071 Speech Patterns (25).cpp

--- AdvanceLevel _C++/1071 Speech Patterns (25).cpp
+++ AdvanceLevel _C++/1071 Speech Patterns (25).cpp
@@ -1,92 +1,35 @@
-#include<stdio.h>
-#include<string.h>
-#include<iostream>
-#include<math.h>
-#include<vector>
-#include<queue>
-#include<algorithm>
-
+#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
-/*
-	printf("%d\n",'a');
-	printf("%d\n",'z');
-	printf("%d\n",'A');
-	printf("%d\n",'Z');
-	printf("%d\n",'0');
-	printf("%d\n",'9');
-*/
-struct word{
-	int num;
-	char word[1000];
-};
-bool chardef(char c){
-	int cc=int(c);
-	if((cc>=97&&cc<=122)||(cc>=65&&cc<=90)||(cc>=48&&cc<=57)) return true;
-	else return false;
-}
-char small(char c){
-	int cc=int(c);
-	if(cc>=65&&cc<=90) return c+32;
-	else return c;
-}
-bool cmp(word a,word b){
-	if(a.num!=b.num) return a.num>b.num;
-	else return a.word[0]<b.word[0];
-}
 int main(){
-//	freopen("1.txt","r",stdin);
-	word A[1000];
-	char temp[1048577];
-	char c;
-	char tempword[1000];
-	cin.getline(temp,1048577);
-	int len=strlen(temp);
-	int in=0;
-	int ain=0;
-	int readin=1;
-	for(int i=0;i<len;i++){
-		c=temp[i];
-		if(chardef(c)&&i!=len-1){
-			tempword[in++]=small(c);
-		}
-		else{
-			if(chardef(c)&&i==len-1) tempword[in++]=small(c);
-			if(in==0) continue;
-			else{
-				for(int j=0;j<ain;j++){
-					if(strcmp(A[j].word,tempword)==0){
-						A[j].num++;
-						readin=0;
-						break;
-					}
-				}
-				if(readin){
-					strcpy(A[ain].word,tempword);
-					A[ain].num=1;
-					ain++;
-					readin=1;
-				}
-				in=0;
-				memset(tempword,0,1000);
+//	freopen("1.txt", "r", stdin);
+	string s;
+	getline(cin,s);
+	map<string, int> mmp;
+	string a = "";
+	int m = 0;int i=0;
+	while(s.length()!=i){
+		string r="";
+        while(i<s.size()){
+            if(isdigit(s[i])==false&&isalpha(s[i])==false){
+                i++;break;
+            }
+            else r+=isdigit(s[i])?s[i]:tolower(s[i]);
+            i++;
+        }
+		if(r!=""){
+			mmp[r]+=1;
+			if(mmp[r]>m){
+				a = r;
+				m = mmp[r];
+			}else if(mmp[r]==m and r<a){
+				a = r;
 			}
 		}
 	}
-	int maxi=0;
-	int maxnum=0;
-	sort(A,A+ain,cmp);
-	if(ain){
-		printf("%s %d",A[0].word,A[0].num);
-	}
-	else printf("0");
-	
+	cout<<a<<" "<<m<<endl;
 	return 0;
-	
-//	for(int i=0;i<ain;i++){
-//		if(A[i].num>maxnum){
-//			maxi=i;
-//			maxnum=A[i].num;
-//		}
-//	}	
-//	printf("%s %d",A[maxi].word,maxnum);
-} 
+}
