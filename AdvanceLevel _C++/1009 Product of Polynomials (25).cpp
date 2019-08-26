8ad725a456abf9c3043303f8b88a4b3dce0dc94c
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2019-08-26 22:40:35 +0800

    Update

--- AdvanceLevel _C++/1009 Product of Polynomials (25).cpp
+++ AdvanceLevel _C++/1009 Product of Polynomials (25).cpp
@@ -1,67 +1,34 @@
 #include <stdio.h>
-#include <math.h>
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main(){
-	double A[1100]={0};
-	double B[1100]={0};
-	double R[3000]={0};
-	int scannum;
-	int scanexp;
-	scanf("%d",&scannum);
-	for(int i=0;i<scannum;i++){
-		
-		scanf("%d",&scanexp);
-		scanf("%lf",&A[scanexp]);
-	}
-	scanf("%d",&scannum);
-	for(int i=0;i<scannum;i++){
-		
-		scanf("%d",&scanexp);
-		scanf("%lf",&B[scanexp]);
-	}
-	
-//	int maxexpA=0,maxexpB=0;
-//	for(int i=0;i<1050;i++){
-//		if(A[i]>maxexpA) maxexpA=i;
-//		if(B[i]>maxexpB) maxexpB=i;
-//	}
-//	//乘积最大系数；
-//	
-//	int maxexpR=maxexpA+maxexpB;
-//	int expa,expb;
-//	
-//	R[maxexpR]=A[maxexpA]*B[maxexpB];
-	for(int i=0;i<1002;i++){
-		for(int j=0;j<1002;j++){
-			if(A[i]&&B[j]){
-				R[i+j]+=A[i]*B[j];
-			}
-		}
-	}
-//	for(int i=2002;i>=0;i--){
-//		for(expa=0;expa<2002;expa++){
-//			expb=i-expa;
-//			if(A[expa]&&B[expb]){
-//				R[i]+=A[expa]*B[expb];
-//			}
-//		}
-//	} 
-	
-	//输出
-	int numR=0;
-	for(int i=2000;i>=0;i--){
-		if(R[i]!=0.0) numR++; 
-	}
-	
-	printf("%d",numR);
-	for(int i=2000;i>=0;i--){
-		if(fabs(R[i])>10e-2){
-			printf(" %d %.1f",i,R[i]);
-		}
-	} 
-	
-	
-	return 0;
-	
+//    freopen("1.txt","r",stdin);
+    int n1,n2,e,mx=0;
+    double p[2500]={0},res[2500]={0},t;
+    cin>>n1;
+    for(int i=0;i<n1;i++){
+        cin>>e;
+        mx=max(mx,e);
+        cin>>p[e];
+    }
+    cin>>n2;
+    for(int i=0;i<n2;i++){
+        cin>>e>>t;
+        for(int j=0;j<=mx;j++){
+            if(p[j]!=0) res[e+j]+=(t*p[j]);
+        }
+    }
+    mx=0;
+    int cnt=0;
+    for(int i=0;i<2500;i++){
+        if(res[i]!=0){
+             mx=i;cnt++;
+        }
+    }
+    cout<<cnt;
+    for(int i=mx;i>=0;i--){
+        if(res[i]!=0) printf(" %d %.1f",i,res[i]);
+    }
 }
-
--- AdvanceLevel _C++/1018 Public Bike Management (30).cpp
+++ AdvanceLevel _C++/1018 Public Bike Management (30).cpp
@@ -1,36 +1,33 @@
 #include <stdio.h>
 #include <iostream>
-#include <map>
 #include <vector>
-#include <string>
 #include <algorithm>
 #define INF 0x3fffffff
-
 using namespace std;
 
-int c,n,sp,m;
-int d[520],g[520][520],in=0,ansi,ansout=INF,ansback=INF,tout,tback;
-vector<int> oribike,pre[520],temp,res[100000];
+int c,m,n,sp;
+vector<int> amt,pre[520];
+int g[520][520];
+int d[520];
 bool vis[520];
-
-void dijkstra(int start){
-    d[start]=0;
-    for(int i=0;i<=n;i++){
-        int u=-1,MIN=INF;
-        for(int j=0;j<=n;j++){
-            if(d[j]<MIN&&vis[j]==false){
-                u=j;MIN=d[j];
+void dijkstra(){
+    d[0]=0;
+    for(int i=0;i<n+1;i++){
+        int u=-1,dis=INF;
+        for(int j=0;j<n+1;j++){
+            if(vis[j]==false&&d[j]<dis){
+                u=j;dis=d[j];
             }
         }
         if(u==-1) return;
         vis[u]=true;
-        for(int j=0;j<=n;j++){
-            if(vis[j]==false&&g[u][j]<INF){
-                if(d[u]+g[u][j]<d[j]){
-                    d[j]=d[u]+g[u][j];
+        for(int j=0;j<520;j++){
+            if(g[u][j]<INF&&vis[j]==false){
+                if(g[u][j]+d[u]<d[j]){
+                    d[j]=g[u][j]+d[u];
                     pre[j].clear();
                     pre[j].push_back(u);
-                }else if(d[u]+g[u][j]==d[j]){
+                }else if(d[j]==g[u][j]+d[u]){
                     pre[j].push_back(u);
                 }
             }
@@ -38,64 +35,58 @@ void dijkstra(int start){
     }
 }
 
+vector<int> psb[1000];
+vector<int> temp;
+int ind=0;
 void dfs(int s,int e){
     if(s==e){
-        temp.push_back(s);
-        res[in]=temp;
-        reverse(res[in].begin(),res[in].end());
-        in++;
-        temp.pop_back();
+        psb[ind++]=temp;
         return;
     }
-    for(int i=0;i<pre[s].size();i++){
-        temp.push_back(s);
-        dfs(pre[s][i],e);
+    for(int i=0;i<pre[e].size();i++){
+        temp.push_back(pre[e][i]);
+        dfs(s,pre[e][i]);
         temp.pop_back();
     }
-    return;
-}
-
-void calbike(int n){
-    int num;
-    int x=0,y=0;
-    for(int i=1;i<res[n].size();i++){
-        num=res[n][i];
-        x=x+oribike[num]-c/2;
-        if(x<0){
-            y=y-x;
-            x=0;
-        }
-    }
-    tout=y;tback=x;
 }
 
 int main(){
 //    freopen("1.txt","r",stdin);
     scanf("%d%d%d%d",&c,&n,&sp,&m);
-    oribike.resize(n+1);
-    for(int i=1;i<=n;i++) scanf("%d",&oribike[i]);
-    fill(d,d+520,INF);
+    amt.resize(n+1);
     fill(g[0],g[0]+520*520,INF);
-    int a,b,dis;
+    fill(d,d+520,INF);
+    fill(vis,vis+520,false);
+    int s,e,dis;
+    for(int i=1;i<=n;i++) scanf("%d",&amt[i]);
     for(int i=0;i<m;i++){
-        scanf("%d%d%d",&a,&b,&dis);
-        g[a][b]=g[b][a]=min(g[a][b],dis);
+        scanf("%d%d%d",&s,&e,&dis);
+        g[s][e]=min(g[s][e],dis);
+        g[e][s]=g[s][e];
     }
-    dijkstra(0);
-    dfs(sp,0);
-
-    for(int i=0;i<in;i++){
-        calbike(i);
-        if(tout<ansout){
-            ansi=i;ansout=tout;ansback=tback;
-        }else if(tout==ansout&&tback<ansback){
-            ansi=i;ansback=tback;
+    dijkstra();
+    temp.push_back(sp);
+    dfs(0,sp);
+    int resn=INF,resi=0,resb;
+    for(int i=0;i<ind;i++){
+        reverse(psb[i].begin(),psb[i].end());
+        int x=0,to=0;
+        for(int j=1;j<psb[i].size();j++){
+            int nxt=psb[i][j];
+            x=x+amt[nxt]-c/2;
+            if(x<0){
+                to=to-x;
+                x=0;
+            }
+        }
+        if(to<resn||(to==resn&&x<resb)){
+            resi=i;
+            resn=to;
+            resb=x;
         }
     }
-    printf("%d ",ansout);
-    for(int i=0;i<res[ansi].size();i++){
-        printf("%d",res[ansi][i]);
-        if(i!=res[ansi].size()-1) printf("->");
-    }
-    printf(" %d",ansback);
+    printf("%d 0",resn);
+    for(int i=1;i<psb[resi].size();i++) printf("->%d",psb[resi][i]);
+    printf(" %d",resb);
+    return 0;
 }
