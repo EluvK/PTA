adbbcaf558b94dce46b53f54aae0d236fd702be0
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2019-08-27 16:33:16 +0800

    Update

--- AdvanceLevel _C++/1049 Counting Ones (30).cpp
+++ AdvanceLevel _C++/1049 Counting Ones (30).cpp
@@ -1,65 +1,53 @@
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
 #include <algorithm>
 #include <math.h>
-
+#define INF 0x3fffffff
 using namespace std;
-int getones(int n);
-int range(int len,int n); 
-int callen(int n){//计算n的位数
-	int count=0;
-	while(n){
-		count++;
-		n/=10;
-	}
-	return count;
+
+int cntone(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        int temp=i;
+        while(temp!=0){
+            if(temp%10==1) sum++;
+            temp/=10;
+        }
+    }
+    return sum;
+}
+int callen(int n){
+    int len=0;
+    do{
+        len++;
+        n/=10;
+    }while(n!=0);
+    return len;
+}
+int tpow(int n){
+    int sum=1;
+    for(int i=0;i<n-1;i++) sum*=10;
+    return sum;
 }
 
-int getonesbylen(int len){
-    //计算n位数内（1-9999）中1出现的个数
-    //规律：
-	return len*pow(10,len-1);
+int del(int n,int len){
+    int temp=tpow(len);
+    return n%temp;
 }
-
-int numof(int i){
-    //暴力迭代
-	int len=callen(i);
-	int sum=0;
-	while(len--){
-		if(i%10==1) sum++;
-		i/=10;
-	}
-	return sum;
+int f(int n){
+    while(n>9) n/=10;
+    return n;
 }
-
-int getones(int n){
-    //计算n以内1出现的个数；
-	if(n>100){//暴力迭代的范围控制
-		int len=callen(n);
-		return getonesbylen(len-1)+range(len,n); 
-	}
-	else{
-		int sum=0;
-		for(int i=1;i<=n;i++){
-			sum+=numof(i);
-		}
-		return sum;
-	}
-}
-int range(int len,int n){
-	//计算len位数字到n这个范围内1出现的个数
-    //10000-23405 len=5
-	int num=pow(10,len-1);//10000
-	int firstnum=n/num;//2
-	int leftnum=n-num*firstnum;//3495
-	if(firstnum==1){
-		return (leftnum)+getones(leftnum)+1;
-	}
-	else return num+(firstnum-1)*getonesbylen(len-1)+getones(leftnum);
+int calone(int n){
+    if(n<100) return cntone(n);
+    int len=callen(n);
+    return calone(del(n,len))+f(n)*(len-1)*tpow(len-1)+min((n-tpow(len)+1),tpow(len));
 }
 
 int main(){
-	int n;
-	scanf("%d",&n);
-	printf("%d",getones(n));
-    return 0;
-} 
+    int n,sum=0;
+    cin>>n;
+    cout<<calone(n);
+}
